f2c36f31f587517a0462a1e356293df3d627ea71
trucedanimal@gmail.com <trucedanimal@gmail.com@79be8460-1c8e-a2a3-18d7-e0e6b998143b>
2014-01-25 18:10:56 +0000

    Mostly additional text for javadoc and some corrections in the wording.

--- EnumBitSet/src/ch/claude_martin/enumbitset/MoreThan64ElementsException.java
+++ EnumBitSet/src/ch/claude_martin/enumbitset/MoreThan64ElementsException.java
@@ -5,6 +5,19 @@ package ch.claude_martin.enumbitset;
  * Unchecked exception for any method that uses a bit set of type long (64 bits)
  * even though the enum type contains more than 64 elements.
  * 
+ * <p>
+ * It is recommended that a static block with an assertion is included to any
+ * enum type that is not indented to contain more than 64 elements:
+ * 
+ * <pre><code>
+ * public static enum Suit implements EnumBitSetHelper&lt;Suit&gt; {
+ *   CLUBS, DIAMONDS, HEARTS, SPADES;
+ *   static {
+ *     assert values().length &lt;= 64 : "This enum type is not indented to contain more than 64 constants.";
+ *   }
+ * }
+ * </code></pre>
+ * 
  * @author <a href="http://claude-martin.ch/enumbitset/">Copyright &copy; 2014
  *         Claude Martin</a>
  */
@@ -13,7 +26,8 @@ public class MoreThan64ElementsException extends RuntimeException {
 	private final Class<? extends EnumBitSetHelper<?>> enumType;
 
 	MoreThan64ElementsException(final Class<? extends EnumBitSetHelper<?>> type) {
-		super("MoreThan64ElementsException for Enum " + type.getSimpleName());
+		// "type" must not be null! But let's be sure this wouldn't fail for null.
+		super("Enum " + (type == null ? "<unknown>" : type.getSimpleName()) + " contains more than 64 elements.");
 		this.enumType = type;
 	}
 
