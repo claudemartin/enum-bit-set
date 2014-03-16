01bef92f22ea6bf81838c278b013b8279f6fce32
trucedanimal@gmail.com <trucedanimal@gmail.com@79be8460-1c8e-a2a3-18d7-e0e6b998143b>
2014-03-16 10:37:50 +0000

    EnumDomain: New implementation of Domain.

---
+++ EnumBitSet/src/ch/claude_martin/enumbitset/EnumDomain.java
@@ -0,0 +1,127 @@
+package ch.claude_martin.enumbitset;
+
+import static java.util.Arrays.asList;
+
+import java.lang.ref.SoftReference;
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class EnumDomain<E extends Enum<E> & EnumBitSetHelper<E>> extends AbstractList<E> implements
+    Domain<E> {
+  // Array of all elements in the domain:
+  private final E[]           elements;                     // index == ordinal
+  private final int           hash;
+  private final Class<E>      enumType;
+
+  private static final// domainCache:
+  Map<Class<? extends Enum<?>>, // Maps Enum-Type to Domain
+  SoftReference<// Allows GC to collect unused Domains
+  Domain<? extends Enum<?>>>> domainCache = new HashMap<>();
+
+  @SuppressWarnings("unchecked")
+  static <X extends Enum<X> & EnumBitSetHelper<X>> Domain<X> of(final Class<X> enumType) {
+    synchronized (domainCache) {
+      Domain<X> domain = null;
+      final SoftReference<Domain<? extends Enum<?>>> ref = domainCache.get(enumType);
+      if (ref != null)
+        domain = (Domain<X>) ref.get();
+      if (null == domain) {
+        domain = new EnumDomain<>(enumType);
+        domainCache.put(enumType, new SoftReference<Domain<? extends Enum<?>>>(domain));
+      }
+      return domain;
+    }
+  }
+
+  private EnumDomain(final Class<E> enumType) {
+    this.enumType = enumType;
+    this.elements = enumType.getEnumConstants();
+    this.hash = Arrays.hashCode(this.elements);
+  }
+
+  @Override
+  public boolean add(final E e) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void add(final int index, final E element) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void clear() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean contains(final Object o) {
+    if (o == null)
+      return false;
+    return o.getClass().getDeclaringClass() == this.enumType;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null || !(obj instanceof Domain) || this.hashCode() != obj.hashCode())
+      return false;
+    if (obj instanceof EnumDomain) {
+      final EnumDomain<?> ed = (EnumDomain<?>) obj;
+      return ed.enumType == ed.enumType && Arrays.equals(this.elements, ed.elements);
+    }
+    return Arrays.equals(this.elements, ((Domain<?>) obj).toArray());
+  }
+
+  @Override
+  public E get(final int index) {
+    return this.elements[index];
+  }
+
+  @Override
+  public int hashCode() {
+    return this.hash;
+  }
+
+  @Override
+  public int indexOf(final Object o) {
+    if (o instanceof Enum)
+      return ((Enum<?>) o).ordinal();
+    return -1;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return this.elements.length == 0;
+  }
+
+  @Override
+  public Iterator<E> iterator() {
+    return asList(this.elements).iterator();
+  }
+
+  @Override
+  public int lastIndexOf(final Object o) {
+    return this.indexOf(o);
+  }
+
+  @Override
+  public int size() {
+    return this.elements.length;
+  }
+
+  @Override
+  public Object[] toArray() {
+    return this.elements.clone();
+  }
+
+  @Override
+  public <X> X[] toArray(final X[] a) {
+    return super.toArray(a);
+  }
+
+}
